f7cbfe5f2c54801ce10ce89d7df745059c8e586f
jesi <jesileguiza1997@gmail.com>
2025-10-23 22:54:26 -0300

    agregue clase cliente,producto, categoria y marca

---
+++ Dominio/Producto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Stock { get; set; }
+        public int Valor { get; set; }
+ 
+    }
+}
